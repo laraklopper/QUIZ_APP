d9bc2f4aa711e714789ad2a4d2b50f23ab5e7a04
Lara Klopper <laraK@corprecover.co.za>
2026-03-02 15:28:13 +0200

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,48 @@
-import React/*, { useState } */from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import {Routes} from 'react-router-dom'
+// Import icons from lucide-react
+import { Bug } from 'lucide-react';
 
 export default function App() {
-  // const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState(null)
   // const [users, setUsers] = useState([])
-  // const [error, setError] = useState(null)
+  const [userData, setUserData] = useState({
+    username: '',
+    fullName: {
+      firstName: '',
+      lastName: '',
+    },
+    email: '',
+    dateOfBirth: '',
+    admin: false,
+    password:'',
+  })
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [error, setError] = useState(null)
+
   return (
    <>
    <Container role='main'>
      <Row>
         <Col></Col>
-        <Col xs={6}>
+        <Col xs={6} id='stateCol' aria-live='polite'>
           {/* GLOBAL ERROR MESSAGE */}
+          <div id='stateMsg'>
+            {error && <p id='errorMessage'>
+            <Bug size={20} fontWeight={900} aria-hidden='true'/>{error}</p>
+            }
+          </div>
         </Col>
         <Col></Col>
       </Row>
+      {/* =============ROUTES=================== */}
+      <Routes>
+
+      </Routes>
    </Container>
 
 
