023e1147150e7be3be4790db294eec489a59ae03
Lara Klopper <laraK@corprecover.co.za>
2026-03-02 15:52:55 +0200

    loginPage

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <Container>
+        <section id='loginSection'>
+        <Row>
+            <Col>
+                <div>
+                    <div></div>
+                </div>
+            </Col>
+        </Row>
+   <Row>
+        <Col>1 of 3</Col>
+        <Col xs={6}>
+            
+        </Col>
+        <Col>3 of 3</Col>
+      </Row>
+        </section>
+    </Container>
   )
 }
