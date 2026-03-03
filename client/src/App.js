72a1f7bbe74ecd94839d35635cc5f9e0673aa735
Lara Klopper <laraK@corprecover.co.za>
2026-03-03 09:24:15 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ import AddQuiz from './pages/AddQuiz';
 import Users from './pages/Users';
 export default function App() {
   const [currentUser, setCurrentUser] = useState(null)
-  // const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([])
   const [userData, setUserData] = useState({
     username: '',
     fullName: {
@@ -37,20 +37,79 @@ export default function App() {
     // Function to fetch all users
     const fetchUsers = async () => {//Define an async function to fetch all users
       try {
-        const token = localStorage('token')
+        const token = localStorage('token');
+
+        if (!token || !loggedIn) return;
+
+        const response = await fetch(`http://localhost:3001/users/fetchUsers`, {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        })
+        
+        /* Conditional rendering to check if the response
+       is not successful (status code is not in the range 200-299)*/
+        if (!response.ok) {
+          console.error('[ERROR: App.js]: Failed to fetch Clients');//Log an error message in the console for debugging purposes
+          throw new Error("Failed to fetch Clients");//Throw an error message if the GET request is unsuccessful
+        }
+
+        const fetchedUsers = await response.json();//Parse the JSON data from the Response body
+        //Conditional rendering to ensure the data is an array
+        if (Array.isArray(fetchedUsers)) {
+          setUsers(fetchedUsers);
+          setError(null); 
+          console.log(fetchedUsers);
+        }
 
       } catch (error) {
-        
+        console.error(`ERROR: App.js: error fetching users`);
+        setError(`ERROR: App.js: error fetching users: ${error.message}`);// Set the error state to display the error in the UI
       }
     }
 
     // Function to fetch a (current) single user
     const fetchCurrentUser = async () => {
-      
+      try {
+        const token = localStorage.getItem('token');
+         if (!token || !loggedIn) return;// If no token is found, exit the function
+
+         const response = await fetch(`http://localhost:3001/users/me`, {
+          method: 'GET',//HTTP request method
+          mode: 'cors',//Enable Cross-Origin resource sharing mode
+          headers: {
+            'Content-Type': 'application/json',// Specify the Content-Type being sent in the request payload.
+            'Authorization': `Bearer ${token}`// Attach the token in the Authorization header  
+          }
+        })
+
+          /* Conditional rendering to check if the response
+       is not successful (status code is not in the range 200-299)*/
+        if (!response.ok) {
+          console.error(`[ERROR: App.js]: Failed to fetch user details `);//Log an error message in the console for debugging purposes
+          throw new Error("Failed to fetch user details");//Throw an error message if the GET request is unsuccessful
+        }
+
+        const fetchedCurrentUser = await response.json();// Parse the response as JSON and set the current user's details
+        
+        setCurrentUser(fetchedCurrentUser)// Update state with fetched user details
+       // console.log(fetchedCurrentUser);
+      } catch (error) {
+        console.error('[ERROR: App.js]: Error fetching current user details');
+        setError(`Error fetching user details: ${error.message}`)
+      }
+    }
+
+    if (loggedIn) {
+      fetchCurrentUser();
+      fetchUsers()
     }
 
 
-  },[])
+  },[loggedIn, setError])
   //=================================================
   return (
    <>
