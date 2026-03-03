58513d08e7904684f3bedb90bd914abd618598a0
Lara Klopper <laraK@corprecover.co.za>
2026-03-03 09:50:21 +0200

    header

--- client/src/components/MainHeader.js
+++ client/src/components/MainHeader.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import '../css/componentCSS/Header.css';
+import Row from 'react-bootstrap/Row'; // Import the Row component from react-bootstrap
+import Col from 'react-bootstrap/Col'; // Import the Col component from react-bootstrap
+import Stack from 'react-bootstrap/Stack';
 
 export default function MainHeader() {
   return (
-    <div>MainHeader</div>
+    <header id='header'>
+        <Row>
+            <Col>
+                  <Stack direction="horizontal" gap={3}>
+                    <div className="p-2">
+                        {/* Header Clock */}
+                    </div>
+                    <div className="p-2 ms-auto">Second item</div>
+                    <div className="p-2">Third item</div>
+                 </Stack>
+            </Col>
+        </Row>
+        <Row>
+            <Col></Col>
+            <Col xs={5}></Col>
+            <Col></Col>
+        </Row>
+        <Row>
+            <Col>
+                <div>
+                    <div></div>
+                </div>
+            </Col>
+        </Row>
+    </header>
   )
 }
