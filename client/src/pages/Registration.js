345f04b90f1451ee824ebd2c66ce04da2a5397d8
Lara Klopper <laraK@corprecover.co.za>
2026-03-03 13:49:08 +0200

    registration

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import MainHeader from '../components/MainHeader';
 
 export default function Registration() {
   return (
-    <div>Registration</div>
+    <Container id='registrationContainer' role='main'>
+      <MainHeader mainHeading={'REGISTRATION'}/>
+      <section id='regisSection'>
+        {/* Registration Form */}
+      </section>
+    </Container>
   )
 }
