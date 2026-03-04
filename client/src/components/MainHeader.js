0d8369c7b65bc6d3b3c84baa0c6a5e3e339eaa96
Lara Klopper <laraK@corprecover.co.za>
2026-03-04 09:07:56 +0200

    changes

--- client/src/components/MainHeader.js
+++ client/src/components/MainHeader.js
@@ -7,7 +7,7 @@ import Stack from 'react-bootstrap/Stack';
 import { Link } from 'react-router-dom';
 // Import icons from lucide-react
 import { UserLock, ChartBarBig, LogIn, Calendar, Clock8 } from 'lucide-react';
-import { dateDisplay } from '../utilFunctions/dateFunctions';
+import { dateDisplay, timeDisplay } from '../utilFunctions/dateFunctions';
 
 export default function MainHeader({mainHeading}) {
     const [headerDate, setHeaderDate] = useState()
@@ -34,11 +34,14 @@ export default function MainHeader({mainHeading}) {
                         {/* Header Clock */}
                         <div id='headerClock' aria-label='Current Date and Time'>                   
                             <ListGroup variant="flush" id='dateTimeList'>
-                                <ListGroup.Item>
+                                <ListGroup.Item id='dateItem'>
                                     <h5 className='clockListIcon'><Calendar size={28}/></h5>
                                     <h5 className='clockListText'>{dateDisplay(headerDate)}</h5>
                                 </ListGroup.Item>
-                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                <ListGroup.Item id='timeItem'>
+                                    <h5 className='clockListIcon'><Clock8 size={28}/></h5>
+                                    <h5 className='clockListText'>{timeDisplay(headerDate)}</h5>
+                                </ListGroup.Item>
                             </ListGroup>
                         </div>
                     </div>
@@ -47,10 +50,10 @@ export default function MainHeader({mainHeading}) {
                         <nav className='navigation'>
                             <ul id='loginNavbar'>
                                 <li className='linkItem'>
-                                    <Link className='refLink' to='/'>LOGIN</Link>
+                                    <Link className='refLink' to='/'><LogIn className='linkIcon'/>LOGIN</Link>
                                 </li>
                                 <li className='linkItem'>
-                                    <Link className='refLink' to='/reg'>REGISTRATION</Link>
+                                    <Link className='refLink' to='/reg'><ChartBarBig className='linkIcon'/>REGISTRATION</Link>
                                 </li>   
                             </ul>
                         </nav>
