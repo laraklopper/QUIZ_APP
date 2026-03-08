83daec0357dddfaa9dbc25e72cdfb273c1228950
Lara Klopper <laraK@corprecover.co.za>
2026-03-08 10:08:24 +0200

    changes

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
-import { Calendar, Clock8, LogOut } from 'lucide-react';
+import { Calendar, Clock8, LogOut, ShieldUser, User} from 'lucide-react';
 import { Copyright } from 'lucide-react';
 import { dateDisplay, timeDisplay } from '../utilFunctions/dateFunctions';
 
@@ -31,7 +31,34 @@ export default function Footer({currentUser, logout}) {
                         USERNAME
                         lOGGED IN AS USER/ADMIN
                     */}
-                    
+                    {currentUser && (
+                      <div>
+<ul id='footerStatusList'>
+                        <li className='footerListItem'>
+                          <span id='footerL'>
+                            <h5 className='footerStatusLabel'></h5>
+
+                          </span>
+                        </li>
+                         {/* User Admin status */}
+                                  <li className='footerListItem'> 
+                                    <span id='adminDetails'>
+                                          <h5 id='status'>Logged in as:</h5>
+                                              {/*Display based on whether or not user is admin  */}
+                                        <h5 id='adminStatus'>
+                                          {currentUser.admin ? (
+                                            // Display if admin user
+                                            <>< ShieldUser style={{ marginRight: 6 }} aria-hidden='true'/>Admin </>
+                                            ) : (
+                                              // Display if normal user
+                                            <><User style={{ marginRight: 6 }} />USER</>
+                                            )}</h5>
+                                    </span>
+                                  </li>
+                      </ul>
+                      </div>
+                      
+                    )}
                 </div>
                 <div className="p-2 ms-auto"></div>
                 <div className="p-2" id='dateTimeBlock'>
