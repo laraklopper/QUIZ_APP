5d8114333385d54df873f353ebda4035daa4c160
Lara Klopper <laraK@corprecover.co.za>
2026-03-08 10:50:08 +0200

    edit user forms

---
+++ client/src/components/EditPasswordForm.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../css/componentCSS/FormSetup.css'
+import '../css/componentCSS/EditUserForms.css'
+export default function EditPasswordForm() {
+  return (
+    <div>EditPasswordForm</div>
+  )
+}
---
+++ client/src/components/EditUserForm.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../css/componentCSS/FormSetup.css'
+import '../css/componentCSS/EditUserForms.css'
+export default function EditUserForm() {
+  return (
+    <div>EditUserForm</div>
+  )
+}
