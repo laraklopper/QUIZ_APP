be5f300a789bc34fbd7974653a173c5c4bd0c2f6
Lara Klopper <laraK@corprecover.co.za>
2026-03-08 12:01:30 +0200

    changes

--- client/src/components/EditPasswordForm.js
+++ client/src/components/EditPasswordForm.js
@@ -16,7 +16,7 @@ export default function EditPasswordForm() {
         <div id='editPswdBlock'>
             <Stack gap={3} id='currentPasswordInputStack' aria-label='password input stack'>
                 <div className="p-2">
-                    <h3 id='formHeading'>EDIT PASSWORD</h3>
+                    <h3 className='formHeading'>EDIT PASSWORD</h3>
                 </div>
                 <div className="p-2" id='currentPswdBlock'>
                     <label htmlFor='currentPswdInput'>
@@ -40,7 +40,7 @@ export default function EditPasswordForm() {
                         id='newPasswordInput'
                     />
                 </div>
-                <div className="p-2">
+                <div className="p-2" id='showNewPswdBlock'>
                     <Button variant='warning' type='button' id='showNewPswdBtn'>
                         SHOW PASSORD
                     </Button>
@@ -52,7 +52,7 @@ export default function EditPasswordForm() {
             </Stack>
         </div>
             <Stack gap={2} className="col-md-5 mx-auto" id='submitPswdChangeBlock'>
-                <Button variant="danger">CLEAR FORM</Button>
+                <Button variant="danger" id='clearFormBtn'>CLEAR FORM</Button>
                 <Button variant="light" id='editPswdBtn' type='submit'>EDIT PASSWORD</Button>
             </Stack>
   
