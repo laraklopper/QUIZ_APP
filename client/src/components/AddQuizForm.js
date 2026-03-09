93b7641cb9ee87bdd8cd00eb270b6018bf2f8093
Lara Klopper <laraK@corprecover.co.za>
2026-03-09 09:49:27 +0200

    addQuiz

--- client/src/components/AddQuizForm.js
+++ client/src/components/AddQuizForm.js
@@ -1,12 +1,65 @@
 // AddQuizForm.js
 import React from 'react'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-export default function AddQuizForm() {
+import '../css/componentCSS/FormSetup.css'
+import '../css/componentCSS/QuizForms.css'
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import { Asterisk} from 'lucide-react';
+export default function AddQuizForm({currentUser}) {
   return (
-    <div id='addNewQuiz'>
+    <div id='newQuizForm'>
+      <form id='addQuizForm' method='POST' aria-labelledby='addQuizFormHeading'>
+      {/* Screen Reader Heading */}
+        <h4 className='visually-hidden' id='addQuizFormHeading'>Add Quiz Form</h4>
+         <Stack gap={3} id='newQuizStack1'>
+      <div id='addQuizHeading'><h3 className='formHeading'>ADD QUIZ</h3></div>
+      <div className="p-2" id='quizUsername' hidden>
+      <input
+        className='input'
+        value={`username: ${currentUser?.username || ''}`}
+        id='quizUsername'
+        readOnly
+      />
+      </div>
+      {/* QUIZ NAME */}
+      <div className="p-2" id='newQuizNameBlock'>
+        <label className='newQuizLabel'>
+          <p className='labelText'>QUIZ NAME:</p>
+          <input
+            className='input'
+            id='newQuizNameInput'
 
+          />
+          <Asterisk size={16} color='red' aria-hidden='true'/>
+        </label>
+
+      </div>
+      
+    </Stack>
+        <div>
+            <Stack gap={3}>
+              <div className="p-2">
+                 <label>
+                  <p className='labelText'></p>
+                 </label>
+              </div>
+              <div className="p-2"></div>
+              <div className="p-2"></div>
+            </Stack>
+            <Stack direction="horizontal" gap={3} id='newQuestionBtnStack'>
+              <div className="p-2">
+                <p className='infoMsg'><small><Asterisk color='red' size={12} /> Indicates required information</small></p>
+              </div>
+              <div className="p-2 ms-auto"><Button variant="light" id='newQuestionBtn'>ADD QUESTION</Button></div>
+              <div className="p-2"><Button variant="danger" id='clearFormBtn'>CLEAR</Button></div>
+            </Stack>
+         
+        </div>
+      </form>
+      {/* New Questions List component
+      include add Quiz Button
+      */}
     </div>
+   
   )
 }
--- client/src/pages/AddQuiz.js
+++ client/src/pages/AddQuiz.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import '../css/pagesCSS/AddQuiz.css'
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import AddQuizForm from '../components/AddQuizForm';
 export default function AddQuiz({logout, currentUser}) {
   return (
     <Container>
@@ -11,8 +16,24 @@ export default function AddQuiz({logout, currentUser}) {
       {/* Quiz list + edit quiz form */}
       {/* Edit quiz instructions */}
       </section>
-      <section>
+      <section id='newQuizSection'>
         {/* ADD QUIZ FORM */}
+        <Row id='addQuizRow'>
+        <Col xs={4} md={2}></Col>
+        <Col xs={12} md={8} id='addQuizCol'>
+        <div>
+          {/* Button to toggle AddQuizForm */}
+          <Button variant="info" id='toggleAddQuizBtn' type='button'>ADD QUIZ FORM</Button>
+        </div>
+        {/* Only display if form is displayed */}
+            <div id='add-quiz-panal'>
+                <AddQuizForm currentUser={currentUser}/>
+            </div>
+        </Col>
+        <Col xs={4} md={2}></Col>
+         
+        </Row>
+        
       </section>
       {/* FOOTER */}
       <Footer logout={logout} currentUser={currentUser}/>
