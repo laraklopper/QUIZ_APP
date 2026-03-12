937b806cbf0fad0a27880dc012a67d47edbe4c3b
Lara Klopper <laraK@corprecover.co.za>
2026-03-12 12:54:21 +0200

    props

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,7 +1,22 @@
 // Quiz.js
 import React from 'react'
 
-export default function Quiz() {
+export default function Quiz(
+  {
+    quiz,
+    quizIndex,
+    setQuizIndex,
+    setCurrentScore,
+    currentScore,
+    questions,
+    quizTimer,
+    timer,
+    // FUNCTIONS
+    handleNextQuestion,
+    handleRestart
+
+  }
+) {
   return (
     <div>Quiz</div>
   )
