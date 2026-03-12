3209ae13f2fd02b061b2b143074847815df5eaf1
Lara Klopper <laraK@corprecover.co.za>
2026-03-12 13:15:16 +0200

    results

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,7 +1,19 @@
 // Results.js
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Results() {
+export default function Results(
+  {
+    currentScore,
+    quizName,
+    totalQuestions,
+    currentUser,
+    addScore
+  }) {
+    const [showScore, setShowScore] = useState(true);
+    const [submitted, setSubmitted] = useState(false)
+    const [submissionError, setSubmissionError] = useState(null);
+
+    const
   return (
     <div>Results</div>
   )
