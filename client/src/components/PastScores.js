577a632c363904c412fc2d73f5344c75cc5534ca
Lara Klopper <laraK@corprecover.co.za>
2026-03-13 11:15:00 +0200

    table

--- client/src/components/PastScores.js
+++ client/src/components/PastScores.js
@@ -47,32 +47,26 @@ export default function PastScores({
                   </FormSelect>
                   {/* Display quizResults scores for the selected quiz */}
                   {selectedQuiz && quizResults.length > 0 ? (
-                    quizResults.map((score, index) => (
-                      <div key={score._id || index} id='scoreDisplay'>
-                          <div className='pastScoreField'> 
-                            {/* Quiz Name */}
-                            <label className='scoreLabel'>QUIZ NAME:</label>
-                            <p className='scoreText'>{score.quizTitle}</p>
-                        </div>
-                        <div className='pastScoreField'>
-                          {/* Display Highest Score for selected quiz */}
-                          <label className='scoreLabel'>SCORE:</label>
-                          <p className='scoreText'>Score: {score.score}</p>
-                        </div>
-                        <div className='pastScoreField'>
-                          {/* Date of highest score */}
-                          <label className='scoreLabel'>DATE:</label>
-                          <p className='scoreText'>
-                            {new Date(score.attemptDate).toLocaleDateString()}
-                          </p>
-                        </div>
-                        <div className='pastScoreField'>
-                          {/* Total number of attempts for the quiz */}
-                            <label className='scoreLabel'>TOTAL ATTEMPTS:</label> 
-                            <p className='scoreText'>{score.attempts}</p>
-                          </div> 
-                      </div>
-                    ))
+                    <table id='scoresTable'>
+                      <thead>
+                        <tr>
+                          <th>QUIZ NAME</th>
+                          <th>SCORE</th>
+                          <th>DATE</th>
+                          <th>TOTAL ATTEMPTS</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {quizResults.map((score, index) => (
+                          <tr key={score._id || index}>
+                            <td>{score.quizTitle}</td>
+                            <td>{score.score}</td>
+                            <td>{new Date(score.attemptDate).toLocaleDateString()}</td>
+                            <td>{score.attempts}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
                   ) : (
                     //Message if no scores are available for the specific quiz
                     selectedQuiz && <p className="scoreError"></p>
