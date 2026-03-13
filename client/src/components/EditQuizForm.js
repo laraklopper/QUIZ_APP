fba9051d099e0613bb652aba8290ac4251717eb1
Lara Klopper <laraK@corprecover.co.za>
2026-03-13 11:47:42 +0200

    Edit Quiz Form

--- client/src/components/EditQuizForm.js
+++ client/src/components/EditQuizForm.js
@@ -3,21 +3,52 @@ import '../css/componentCSS/FormSetup.css'
 import '../css/componentCSS/QuizForms.css'
 import '../css/componentCSS/QuizData.css'
 import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
 
 export default function EditQuizForm() {
 
+    /* dont allow username edit*/
     //=========JSX RENDERING================
   return (
-    <form>
+    <form id='editQuizForm' aria-labelledby='editQuizData'>
+        {/* -----Screen Reader Heading-------- */}
+        <p className='visually-hidden' id='editQuizData'>EDIT QUIZ</p>
+        {/*  */}
         <div id='editQuizDetails'>
-                <Stack gap={3}>
-      <div className="p-2">First item</div>
-      <div className="p-2">Second item</div>
-      <div className="p-2">Third item</div>
-    </Stack>
+                <Stack gap={3} id='editQuizStack1'>
+                    
+                    {/* TITLE:QuizName */}
+                    <div className="p-2">
+                        <label>
+                            <p className='labelText'></p>
+                        </label>
+                    </div>
+                    {/* QUIZ DESCRIPTION */}
+                    <div className="p-2">
+                         <label>
+                            <p className='labelText'></p>
+                        </label>
+                    </div>
+                    {/* Questions */}
+                    <div className="p-2">
+                        <label>
+                            <p className='labelText'></p>
+                        </label>
 
-
+                    </div>
+                </Stack>
+                 <Stack gap={3} id='editQuizStack2'>
+                    <div className="p-2">First item</div>
+                    <div className="p-2">Second item</div>
+                    <div className="p-2">Third item</div>
+                </Stack>
         </div>
+        <div id='editQuizBtnBlock'>
+            <Stack gap={2} className="col-md-5 mx-auto" id='editQuizStack3'>
+                <Button variant="secondary">Save changes</Button>
+                <Button variant="outline-secondary">Cancel</Button>
+            </Stack>
+        </div>      
     </form>
   )
 }
