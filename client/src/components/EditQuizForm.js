4b4e1520ca1f4b795709590613f8d6b4818131ba
Lara Klopper <laraK@corprecover.co.za>
2026-03-13 11:23:36 +0200

    Edit quiz form setup

---
+++ client/src/components/EditQuizForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../css/componentCSS/FormSetup.css'
+import '../css/componentCSS/QuizForms.css'
+import '../css/componentCSS/QuizData.css'
+
+export default function EditQuizForm() {
+  return (
+    <div>EditQuizForm</div>
+  )
+}
